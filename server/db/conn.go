31c6625874e648272f8defa4787e4c733efe782f
joshlgrossman <josh.l.grossman@gmail.com>
2017-06-19 05:16:08 -0500

    add db connection

---
+++ server/db/conn.go
@@ -0,0 +1,6 @@
+package db
+
+import "database/sql"
+
+// Conn global connection
+var Conn *sql.DB
--- server/server.go
+++ server/server.go
@@ -1,12 +1,25 @@
 package main
 
 import (
+	"database/sql"
 	"net/http"
 
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/joshlgrossman/go-ang/server/db"
 	"github.com/joshlgrossman/go-ang/server/routes"
 )
 
 func main() {
+	var err error
+	db.Conn, err = sql.Open("mysql", "root:@/test")
+	defer db.Conn.Close()
+
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
 
 	http.HandleFunc("/", routes.Static("client/build/"))
 	http.HandleFunc("/ws/test", routes.Test)
