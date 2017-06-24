389978a6b4deca3ab6f99ba030160ea2ea6a1ed9
joshlgrossman <josh.l.grossman@gmail.com>
2017-06-24 13:00:48 -0500

    task form component

---
+++ client/src/scripts/components/task-form/task-form.controller.js
@@ -0,0 +1,30 @@
+app.component('taskForm', {
+
+	templateUrl: './task-form.view.html',
+
+	controller: ['$http', '$scope', 'ws', function($http, $scope, ws){
+		this.task = {}
+
+		this.reset = () => {
+			this.task = {
+				title: '',
+				description: ''
+			}
+		}
+
+		this.submit = () => {
+			$http.post(ws('tasks'), angular.copy(this.task)).then(response => {
+				console.log(response)
+				if(!response.data.error){
+					$scope.$emit('task:added', response.data)
+				}
+			}).catch(console.log)
+			
+			this.reset()
+		}
+
+		this.reset()
+
+	}]
+
+})
--- client/src/scripts/components/tasks/tasks.controller.js
+++ client/src/scripts/components/tasks/tasks.controller.js
@@ -2,10 +2,16 @@ app.component('tasks', {
 
 	templateUrl: './tasks.view.html',
 
-	controller: ['$http', 'ws', function($http, ws) {
+	controller: ['$http', '$scope', 'ws', function($http, $scope, ws) {
+
 		$http.get(ws('tasks')).then(response => {
 			this.tasks = response.data
 		})
+
+		$scope.$on('task:added', (event, task) => {
+			this.tasks.push(task)
+		})
+
 	}]
 
 })
